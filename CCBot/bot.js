3049a009a7ed6c5482764baf1bbfe4b7da4d14ae
cucumbercan <97450306+cucumbercan@users.noreply.github.com>
2022-02-22 13:55:24 +0800

    Update bot.js

--- CCBot/bot.js
+++ CCBot/bot.js
@@ -3,8 +3,8 @@ var dic=[
 	['hi','','hi~'],
 	['你好','','你也好！'],
 	['你','名字','我叫Cucumber.'],
-	['不智能','','对不起，我还在试验阶段！<br>你可以进入<a href=\'https://cucumbercan.github.io/ChatBot/FB.html\'>FeedBack中心</a>提交你的意见！'],
-	['人工','智障','对不起，我还在试验阶段！<br>你可以进入<a href=\'https://cucumbercan.github.io/ChatBot/FB.html\'>FeedBack中心</a>提交你的意见！'],
+	['不智能','','对不起，我还在试验阶段！<br>你可以进入<a href=\'https://cucumbercan.github.io/CCBot/FB.html\'>FeedBack中心</a>提交你的意见！'],
+	['人工','智障','对不起，我还在试验阶段！<br>你可以进入<a href=\'https://cucumbercan.github.io/CCBot/FB.html\'>FeedBack中心</a>提交你的意见！'],
 	// ['','',''],
 ]
 
@@ -32,7 +32,7 @@ var HttpClient = function() {
 function bot(){
 	value=document.getElementById("ipt").value;
 	document.getElementById("ipt").value="";
-	var out="你说啥？我不明白。<br>你可以帮助我改进！<br>进入<a href='https://cucumbercan.github.io/ChatBot/FB.html'>FeedBack中心</a>提交你的意见！";
+	var out="你说啥？我不明白。<br>你可以帮助我改进！<br>进入<a href='https://cucumbercan.github.io/CCBot/FB.html'>FeedBack中心</a>提交你的意见！";
 	for(var i=0;i<dic.length;i++)
 	{
 		if(value.indexOf(dic[i][0])!=-1 && value.indexOf(dic[i][1])!=-1){
