59c33909b604901f4d419df98029bef3c92c7b4c
cucumbercan <97450306+cucumbercan@users.noreply.github.com>
2022-02-22 13:07:02 +0800

    Update bot.js

--- CCBot/bot.js
+++ CCBot/bot.js
@@ -3,8 +3,8 @@ var dic=[
 	['hi','','hi~'],
 	['你好','','你也好！'],
 	['你','名字','我叫Cucumber.'],
-	['不智能','','对不起，我还在试验阶段！<br>你可以进入<a href=\'https://cucumbercan.github.io/ChatBot/FB.html\'>FeedBack中心</a>提交你的意见！'],
-	['人工','智障','对不起，我还在试验阶段！<br>你可以进入<a href=\'https://cucumbercan.github.io/ChatBot/FB.html\'>FeedBack中心</a>提交你的意见！'],
+	['不智能','','对不起，我还在试验阶段！<br>你可以进入<a href=\'https://cucumbercan.github.io/CCBot/FB.html\'>FeedBack中心</a>提交你的意见！'],
+	['人工','智障','对不起，我还在试验阶段！<br>你可以进入<a href=\'https://cucumbercan.github.io/CCBot/FB.html\'>FeedBack中心</a>提交你的意见！'],
 	// ['','',''],
 ]
 
