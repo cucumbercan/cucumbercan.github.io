85b3a813c4c64f02ecd566396d26845d06168e34
cucumbercan <97450306+cucumbercan@users.noreply.github.com>
2022-03-02 12:48:04 +0800

    Update s.js

--- CCPlay/s.js
+++ CCPlay/s.js
@@ -1,6 +1,7 @@
 var l=[
 	["BOOMCheer","CC.","apps/BOOMCheer.png","apps/BOOMCheer-ist.exe"],
 	["GimgX","CC.","apps/gimgx.ico","apps/GimgX-ist.exe"],
+	["G-Coin","CC.","apps/G-Coin.exe",""],
 	
 ];
 
