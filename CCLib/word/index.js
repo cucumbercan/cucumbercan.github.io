fc0802e6fd15867f30162849da83506425692494
cucumbercan <97450306+cucumbercan@users.noreply.github.com>
2022-05-01 20:14:37 +0800

    Add files via upload

--- CCLib/word/index.js
+++ CCLib/word/index.js
@@ -5,15 +5,16 @@ function show(){
 		for(var j=0;j<x.other.length;j++){
 			s+="<div class='other'>"+x.other[j]+"</div>";
 		}
-		s+="<br><s>短语：</s>";
+		s+=x.phrase[0]?"<br><s>短语：</s>":"";
 		for(var j=0;j<x.phrase.length;j++){
 			s+="<div class='other'>"+x.phrase[j]+"</div>";
 		}
-		s+="<br><s>例句：</s><br>";
+		s+=x.example[0]?"<br><s>例句：</s><br>":"";
 		for(var j=0;j<x.example.length;j++){
 			s+="<div class='example'>"+x.example[j]+"</div>";
 		}
 		document.getElementById("words").innerHTML+="<div class='word' style='font-color: #FFFFFF;'>"+s+"</div>"
 		// console.log(x)
+		console.log(x.word,x.phrase.length)
 	}
 }
